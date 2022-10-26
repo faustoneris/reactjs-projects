01c705c820985ba4b809bab9cfd2d34b4e376647
Fausto Neris <fausto2102@hotmail.com>
2022-10-26 10:44:47 -0300

    iniciando modulo SPA

---
+++ 02-ignite-timer/src/App.tsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import { Button } from './components/Button'
+
+export function App() {
+  return (
+    <>
+      <Button variant='primary'/>
+      <Button variant='secondary' />
+      <Button variant='success'/>
+      <Button variant='danger'/>
+      <Button/>
+    </>
+  )
+}
+
